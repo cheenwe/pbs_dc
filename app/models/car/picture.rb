74a9c807f1af3e4025bb5ee5e3c0bfb15c90680c
cheenwe <you@example.com>
2020-10-31 10:36:40 +0800

    download pic

--- app/models/car/picture.rb
+++ app/models/car/picture.rb
@@ -4,26 +4,28 @@ class Car::Picture < ApplicationRecord
 
   # Car::Picture.download_all_pic
   def self.download_all_pic
-    download_path = "/tmp/cars/"
-    batch_size = 2
+    download_path = "/disk4/data/cars/"
+    batch_size = 5000
     folder = "00"
-    Car::Picture.find_in_batches(batch_size: batch_size).each do |list|
+    Car::Picture.where("id >?", 54637).find_in_batches(batch_size: batch_size).each do |list|
       current_folder = download_path+folder
-      system("mkdir -p #{current_folder}")
+      system("mkdir -p #{current_folder}") 
 
       list.each do |item|
         p item.id
         url = item.url
         name = System.gen_pic_name
         pic_name = current_folder+"/"+name
+        pic_info = folder +"/"+name
         # p pic_name
         # p url
-        item.update_columns(imgpath: pic_name)
+        item.update_columns(imgpath: pic_info)
         DownloadJob.perform_later(url, pic_name)
 
-        sleep((SecureRandom.rand*10).to_i)
+      #sleep((SecureRandom.rand))
       end
       # return
+      sleep((SecureRandom.rand*10).to_i)
       folder = System.gen_next(folder)
     end
   end
